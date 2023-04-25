dc96f5dc4723e96ca68125461bbc9eea427baca1
chryspham <chrysabel.pham@gmail.com>
2023-04-24 23:57:55 -0700

    added Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <p>
+        This project is coded by Chrys Pham and is{""}
+        <a
+          href="https://marvelous-gecko-4306e4.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github.
+        </a>
+      </p>
+    </div>
+  );
 }
