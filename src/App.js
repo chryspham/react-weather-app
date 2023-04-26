cde14e492ce0d754aef209d45cbdeb42049841b7
chryspham <chrysabel.pham@gmail.com>
2023-04-25 17:00:51 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <p>
-        This project is coded by Chrys Pham and is{""}
-        <a
-          href="https://marvelous-gecko-4306e4.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github.
-        </a>
-      </p>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <p>
+          This project is coded by Chrys Pham and is {""}
+          <a
+            href="https://marvelous-gecko-4306e4.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github.
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
